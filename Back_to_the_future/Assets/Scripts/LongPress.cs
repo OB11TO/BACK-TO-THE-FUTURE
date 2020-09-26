ec871ccd2fa35e73e2028d923efc58b7568a2b95
Cruvadio <ialexandrova53@gmail.com>
2020-09-27 01:30:53 +0300

    Some scretches
    + Added Player
    + Player now can walk right and left and jump by pressing buttons on the screen
    + Player collides ground and platforms
    
    Unfortunately, player is now an alien)
    P.S. Added a lot of assets, I'll delete them later.

---
+++ Back_to_the_future/Assets/Scripts/LongPress.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class LongPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    private bool isDown;
+
+    public UnityEvent onPointerDown;
+
+
+    public void OnPointerDown(PointerEventData eventData) 
+    {
+        this.isDown = true;
+    }
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        this.isDown = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDown)
+            onPointerDown.Invoke();
+    }
+}
---
+++ Back_to_the_future/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    public float velocity;
+
+    public float jumpHeight;
+
+    private bool isMovingForward;
+
+    public Transform checkGround;
+
+    bool isGrounded;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Flip();
+        CheckGround();
+    }
+
+
+    public void MovePlayer (float direction)
+    {
+        if (direction > 0.0f) isMovingForward = true;
+        else isMovingForward = false;
+        rb.velocity = new Vector2(direction * velocity, rb.velocity.y);
+    }
+
+    public void Jump ()
+    {
+        if (isGrounded)
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+    }
+
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(checkGround.position, 0.2f);
+        isGrounded = colliders.Length > 1;
+    }
+
+    private void FixedUpdate()
+    {
+        
+
+        
+    }
+
+    private void Flip()
+    {
+        if (isMovingForward)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+}
