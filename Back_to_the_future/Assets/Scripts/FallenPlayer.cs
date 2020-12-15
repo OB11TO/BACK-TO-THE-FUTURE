c9911cf354c489937ad60ff63dcd7c30a142d1c6
Cruvadio <ialexandrova53@gmail.com>
2020-12-15 09:11:36 +0300

    Completed

---
+++ Back_to_the_future/Assets/Scripts/FallenPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FallenPlayer : MonoBehaviour
+{
+
+    public UnityEvent onPlayerFalling;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Fallen!!");
+            onPlayerFalling.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Back_to_the_future/Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+    public GameObject replayObject;
+    public GameObject winText;
+    public GameObject loseText;
+    public GameObject tapText;
+
+    bool isWon = false;
+    bool isGameOver = false;
+
+    public Player player;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance == this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        replayObject.SetActive(false);
+        winText.SetActive(false);
+        loseText.SetActive(false);
+    }
+
+    public void Play()
+    {
+        replayObject.SetActive(false);
+        winText.SetActive(false);
+        loseText.SetActive(false);
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ShowReplayScreen()
+    {
+        replayObject.SetActive(true);
+    }
+
+    public void WinScreen()
+    {
+        winText.SetActive(true);
+    }
+
+    public void GameOverScreen()
+    {
+        loseText.SetActive(true);
+    }
+
+    public void ActivateTapping(bool enable)
+    {
+        tapText.SetActive(enable);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
